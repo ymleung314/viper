1972fc198d8d5f863632148248f5f27d1023c4f3
Yau-ming Leung <ymleung314@gmail.com>
2025-12-11 15:52:01 +0000

    Save means to file

--- scripts/k_means.py
+++ scripts/k_means.py
@@ -1,13 +1,16 @@
+import struct
+
 import numpy as np
 from sklearn.cluster import KMeans
 
 INPUT_FILE = "/data/ymliang/sample-1.fbin"
-N_CLUSTERS = 2
+OUTPUT_FILE = "/data/ymliang/means-5.fbin"
+N_CLUSTERS = 5
 RANDOM_SEED = 0
 
 
 def main():
-    print("Extracting vectors...")
+    print(f"Extracting vectors from {INPUT_FILE}")
     with open(INPUT_FILE, "rb") as inf:
         n_vec = np.fromfile(inf, dtype=np.int32, count=1)[0]
         vec_dim = np.fromfile(inf, dtype=np.int32, count=1)[0]
@@ -19,7 +22,13 @@ def main():
     print("Running KMeans...")
     kmeans = KMeans(n_clusters=N_CLUSTERS, random_state=RANDOM_SEED)
     kmeans.fit(vecs)
-    print(kmeans.cluster_centers_)
+
+    print(f"Saving means to {OUTPUT_FILE}")
+    with open(OUTPUT_FILE, "wb") as outf:
+        outf.write(struct.pack("<ii", N_CLUSTERS, vec_dim))
+
+        for mean in kmeans.cluster_centers_:
+            np.array(mean, np.float32).tofile(outf)
 
 
 if __name__ == "__main__":
